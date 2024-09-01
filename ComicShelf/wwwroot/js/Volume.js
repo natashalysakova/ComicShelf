ebf17c556ff5053837385536a03279f33dc7eb42
natashalysakova <natashalysakova@gmail.com>
2024-09-02 00:46:18 +0300

    hotfixes

--- ComicShelf/wwwroot/js/Volume.js
+++ ComicShelf/wwwroot/js/Volume.js
@@ -638,10 +638,30 @@ async function createFile(url) {
 
 
     } catch (e) {
-        navigator.clipboard.writeText(url)
+
+        if (window.isSecureContext && navigator.clipboard) {
+            navigator.clipboard.writeText(url);
+        } else {
+            unsecuredCopyToClipboard(url);
+        }
     }
 }
 
 function importBegin(data) {
     $("#import-spinner").show();
+}
+
+
+function unsecuredCopyToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+        document.execCommand('copy');
+    } catch (err) {
+        console.error('Unable to copy to clipboard', err);
+    }
+    document.body.removeChild(textArea);
 }
