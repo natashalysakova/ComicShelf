171022e4b09b73852b631b4531b3a92cca5f346f
natashalysakova <natashalysakova@gmail.com>
2025-01-09 19:33:29 +0200

    update

--- update.sh
+++ update.sh
@@ -4,13 +4,12 @@ IMAGE=ghcr.io/natashalysakova/comic-shelf
 read -p 'Version: ' VERSION
 
 docker build -t ${IMAGE}:${VERSION} . -f ./ComicShelf/Dockerfile --platform linux/arm64 | tee build.log || exit 1
-ID=$(tail -1 build.log | awk '{print $3;}')
-docker tag $ID ${IMAGE}:latest
+docker tag ${IMAGE}:${VERSION} ${IMAGE}:latest
 
 docker images | grep ${IMAGE}
 
 docker image push ${IMAGE}:latest
-docker image psuh ${IMAGE}:{$VERSION}
+docker image push ${IMAGE}:{$VERSION}
 
 docker compose down
 docker compose pull
