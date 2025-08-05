6bf217bccbe5a5c9f56cd46223172474c4721449
natashalysakova <natashalysakova@gmail.com>
2025-08-06 00:23:05 +0300

    fix

--- update.sh
+++ update.sh
@@ -17,14 +17,3 @@ docker image push ${IMAGE}:${VERSION}
 docker compose down
 docker compose pull
 docker compose up -d
-
-docker exec -ti shelf-backend /bin/bash
-
-apt-get update \
-    && apt-get install -y wget gnupg \
-    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
-    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
-    && apt-get update \
-    && apt-get install -y google-chrome-stable fonts-ipafont-gothic fonts-wqy-zenhei fonts-thai-tlwg fonts-kacst fonts-freefont-ttf libxss1 \
-      --no-install-recommends \
-    && rm -rf /var/lib/apt/lists/*
